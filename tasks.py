e707d279e636a96bd6e63697be06cf6b8cf880c6
Andree Villegas <andree.w.villegas@gmail.com>
2026-03-28 17:11:55 -0500

    Código tasks.py

--- tasks.py
+++ tasks.py
@@ -1,2 +1,18 @@
+# Se define la lógica de negocio, cómo se crean y gestionan las tareas.
+from datetime import datetime
+from storage import load_tasks, save_tasks
 
-
+def add_task(description):
+    tasks = load_tasks()
+    new_id = len(tasks) + 1
+    now = datetime.now().isoformat(timespec="seconds")
+    task = {
+        "id": new_id,
+        "description": description,
+        "status": "todo",
+        "createdAt" : now,
+        "updateAt" : now
+    }
+    tasks.append(task)
+    save_tasks(tasks)
+    return f"Tarea añadida exitosamente (ID: {new_id})"
