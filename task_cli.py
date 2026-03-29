767eff2ead7e315bc357ce2a1826385ede631ffc
Andree Villegas <andree.w.villegas@gmail.com>
2026-03-28 22:22:15 -0500

    Agregar función sobre el cambio de estado a task_cli.py

--- task_cli.py
+++ task_cli.py
@@ -1,5 +1,5 @@
 import sys
-from tasks import add_task, list_tasks, update_task, delete_task
+from tasks import add_task, list_tasks, update_task, delete_task, mark_task
 
 def main():
     args = sys.argv[1:]
@@ -11,11 +11,13 @@ def main():
     if command == "add":
         description = " ".join(args[1:])
         print(add_task(description))
+
     elif command == "list":
         if len(args) > 1:
             list_tasks(args[1])
         else:
             list_tasks()
+
     elif command == "update":
         if len(args) < 3:
             print("Uso: update <id> <nueva descripción>")
@@ -23,12 +25,27 @@ def main():
             task_id = int(args[1])
             new_description = " ".join(args[2:])
             print(update_task(task_id, new_description))
+
     elif command == "delete":
         if len(args) < 2:
             print("Uso: delete <id>")
         else:
             task_id = int(args[1])
             print(delete_task(task_id))
-
+    
+    elif command == "mark-in-progress":
+        if len(args) < 2:
+            print("Uso: mark-in-progress <id>")
+        else:
+            task_id = int(args[1])
+            print(mark_task(task_id, "in-progress"))
+    
+    elif command == "mark-done":
+        if len(args) < 2:
+            print("Uso: mark-done <id>")
+        else:
+            task_id = int(args[1])
+            print(mark_task(task_id, "done"))
+    
 if __name__ == "__main__":
     main()
